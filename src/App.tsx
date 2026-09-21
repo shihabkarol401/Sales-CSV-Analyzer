13cbce7a305f719cf6c81edd31c6164aa8810d9f
Shihab <karolshihab@gmail.com>
2026-09-21 17:12:37 +0530

    feat: connect frontend to deployed api

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ function App() {
 
   try {
     const response = await fetch(
-      "http://127.0.0.1:8000/upload",
+      "https://sales-csv-analyzer-api.onrender.com/upload",
       {
         method: "POST",
         body: formData,
